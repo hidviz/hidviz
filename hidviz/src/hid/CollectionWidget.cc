002f138d9096f68af8ca159288024492423cafa8
Ondřej Budai <ondrej@budai.cz>
2017-05-01 14:11:59 +0200

    Use new libhidx API to retrieve usage and type text for Collection

--- hidviz/src/hid/CollectionWidget.cc
+++ hidviz/src/hid/CollectionWidget.cc
@@ -1,7 +1,6 @@
 #include "CollectionWidget.hh"
 
 #include <libhidx/hid/Collection.hh>
-#include <libhidx/Usages.hh>
 
 #include <QLabel>
 
@@ -9,18 +8,14 @@ namespace hidviz {
 namespace hid {
 
     CollectionWidget::CollectionWidget(libhidx::hid::Collection* collection, QWidget* parent) : ItemWidget{collection->getLevel(), parent}, m_collection{collection} {
-        const char* types[] = {
-            "Physical", "Application", "Logical", "Report", "Named Array",
-            "Usage Switch", "Usage modifier"
-        };
 
         auto name = QString {"Collection ("};
-        name += types[m_collection->m_type];
+        name += QString::fromStdString(m_collection->getTypeStr());
         name += ")";
 
         setName(name);
 
-        auto usageText = getHidUsageText(collection->m_usage);
+        auto usageText = collection->getUsageStr();
 
         setUsage(QString::fromStdString(usageText));
     }
